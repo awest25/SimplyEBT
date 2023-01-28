2f98271ae517cb692295810ffb69774d32236f22
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-01-28 14:12:41 -0800

    app wrapper placeholder

---
+++ src/components/AppWrapper.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AppWrapper(props) {
+
+    return (
+        <div>
+            <p>Header</p>
+            <p>Body</p>
+            <p>Footer</p>
+        </div>
+    )
+}
+
+export default AppWrapper
