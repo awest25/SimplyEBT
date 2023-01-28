4de7b8b92d7ed6d2babee09bdff4d90679110089
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-01-28 15:00:45 -0800

    firebase implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import AppWrapper from './components/AppWrapper';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
