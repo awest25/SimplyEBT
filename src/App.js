c3b32166d35669f6cf695af073246a456ea5be07
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-01-28 16:08:30 -0800

    dashboard placeholder

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import AppWrapper from './components/AppWrapper';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Dashboard from './routes/Dashboard';
 
 function App() {
   return (
-    <AppWrapper/>
+    <Dashboard/>
   );
 }
 
---
+++ src/routes/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppWrapper from "../components/AppWrapper";
+
+function Dashboard (){
+    return (
+        <AppWrapper>
+            <p>Dashboard</p>
+        </AppWrapper>
+    );
+}
+
+export default Dashboard
